47bf5707a35168143bf0e59946f8bf945c7aea2c
Ismael0714 <ismaelandrez98@gmail.com>
2022-09-05 22:42:42 -0600

    version 1.1

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php require('./layout/header.php') ?>
+
+<!-- CONTENIDO -->
+
+
+<div class="container-content">
+
+    <article>
+        <h1>Plantilla 1</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident ab illo esse nisi obcaecati
+            voluptates eveniet et, mollitia porro quos! Ut eius eos, aut exercitationem, amet at quae neque
+            voluptate itaque, veniam sunt soluta ratione quis perferendis dolorum. Inventore ratione eaque velit
+            excepturi, eveniet a ab hic provident repellat vel error soluta blanditiis similique tempora cum qui
+            necessitatibus alias nobis quidem? Dolorum autem blanditiis explicabo vitae excepturi, dolores vero,
+            saepe, natus quod aperiam earum alias dolor debitis nemo dignissimos enim at hic repellendus sapiente
+            incidunt sunt minus tempora eum! Ipsam voluptatibus non alias accusamus ducimus ut, commodi labore
+            aliquid? Odio ullam tempora consequuntur illo neque odit velit totam, libero facere vero reiciendis
+            fugit, doloribus fuga non rerum soluta veniam esse nisi. Autem iste repellat quis consequatur veritatis
+            optio impedit illum! Quod unde pariatur aliquam eius tempora ducimus repellat facere quaerat eligendi
+            magnam! Sunt expedita minima ad quo reiciendis blanditiis officiis, voluptatum ab veritatis explicabo
+            ex? Et voluptatum consequatur perspiciatis, culpa consequuntur recusandae? Aliquam aperiam vitae,
+            impedit ex pariatur reiciendis at adipisci incidunt deserunt ullam in eum! Praesentium odit natus facere
+            inventore nemo, error mollitia perspiciatis dolor autem iure vero optio, deserunt sequi quas nam. Fugit
+            totam fuga, dicta ea magnam beatae qui, quod sunt illo animi itaque obcaecati atque molestias! Sit
+            mollitia labore ab facilis modi eligendi, dicta fugiat ratione iusto totam obcaecati natus illum qui,
+            impedit harum aspernatur perferendis eum quos cumque deleniti? Neque doloremque odit est, eius qui
+            labore quos amet sunt quas quisquam veritatis expedita harum laudantium laboriosam alias deserunt
+            officia vel tempore soluta velit. Iste accusantium cum debitis voluptate inventore labore facere iure
+            deleniti tempore quibusdam quia ab natus ullam, suscipit vero quaerat! Distinctio quaerat reprehenderit
+            numquam, nisi quia aperiam. Dolorem quisquam et quidem vero suscipit, cupiditate dolor quos? Incidunt
+            pariatur rerum quas ducimus laboriosam?
+
+            <img src="img/img1.jpg" alt="">
+
+        </p>
+    </article>
+
+
+
+
+    <?php require('./layout/aside.php') ?>
+    <?php require('./layout/footer.php') ?>
---
+++ layout/footer.php
@@ -0,0 +1,26 @@
+<!--FOOTER-->
+
+<div class="container-footer">
+    <footer>
+        <div class="logo-footer">
+            <img src="img/LOGOIC.png" alt="">
+        </div>
+
+        <div class="redes-footer">
+            <a href=""><i class="fab fa-facebook-f icon-redes-footer"></i></a>
+            <a href=""><i class="fab fa-google-plus-g icon-redes-footer"></i></a>
+            <a href=""><i class="fab fa-instagram icon-redes-footer"></i></a>
+
+        </div>
+
+        <hr>
+        <h4>@2022 SoftwareIc - Todos los derechos reservados</h4>
+    </footer>
+</div>
+</div>
+
+
+<script src="js/script.js"></script>
+</body>
+
+</html>
